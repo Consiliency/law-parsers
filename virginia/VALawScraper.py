47748bcaced8db448170d3ff9a10ddc748e16411
ViperJuice <jennertorrence@hotmail.com>
2024-06-17 13:04:40 -0400

    Second Commit

--- virginia/VALawScraper.py
+++ virginia/VALawScraper.py
@@ -1,7 +1,9 @@
 import requests
 import json
 import urllib3
+import os
 from datetime import datetime
+from jsonpath_ng import jsonpath, parse
 
 # Suppress only the single InsecureRequestWarning from urllib3 needed
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -22,68 +24,218 @@ def fetch_data(url):
         print(f"Failed to retrieve {url}: {e}")
         return None
 
-def gather_all_data():
-    all_data = {}
+def gather_administrative_code():
+    
+    ''' API responses of JSON schema
+    {
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "title": "RegulationSchema",
+        "type": "object",
+        "properties": {
+            "TitleNumber": {
+                "type": "string"
+            },
+            "TitleName": {
+                "type": "string"
+            },
+            "AgencyList": {
+                "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "AgencyNumber": {
+                        "type": "string"
+                    },
+                "AgencyName": {
+                    "type": "string"
+                },
+                "Preface": {
+                    "type": "object",
+                    "properties": {
+                        "Preface": {
+                            "type": "string"
+                        },
+                        "PrefaceSummary": {
+                            "type": "string"
+                        }
+                    },
+                    "required": ["Preface", "PrefaceSummary"]
+                },
+                "ChapterList": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "ChapterNumber": {
+                                "type": "string"
+                            },
+                            "ChapterName": {
+                                "type": "string"
+                            },
+                            "Sections": {
+                                "type": "array",
+                                "items": {
+                                    "type": "object",
+                                    "properties": {
+                                        "PartNumber": {
+                                            "type": "string"
+                                        },
+                                        "PartName": {
+                                            "type": "string"
+                                        },
+                                        "ArticleNumber": {
+                                            "type": "string"
+                                        },
+                                        "ArticleName": {
+                                            "type": "string"
+                                        },
+                                        "SectionNumber": {
+                                            "type": "string"
+                                        },
+                                        "SectionTitle": {
+                                            "type": "string"
+                                        },
+                                        "Body": {
+                                            "type": "string"
+                                        },
+                                        "Authority": {
+                                            "type": "string"
+                                        },
+                                        "HistoricalNote": {
+                                            "type": "string"
+                                        }
+                                    },
+                                    "required": ["PartNumber", "PartName", "SectionNumber", "SectionTitle", "Body"]
+                                }
+                            },
+                            "required": ["ChapterNumber", "ChapterName", "Sections"]
+                        }
+                    }
+                },
+                "required": ["AgencyNumber", "AgencyName", "Preface", "ChapterList"]
+                }
+            }
+        },
+        "required": ["TitleNumber", "TitleName", "AgencyList"]
+    }
+    '''
+    # jsonpath expressions to extract data from JSON response with schema above
+    jsonpath_expressions = {
+        "TitleNumber": parse('$.TitleNumber'),
+        "TitleName": parse('$.TitleName'),
+        "AgencyList": parse('$.AgencyList[*]'),
+        "AgencyNumber": parse('$.AgencyList[*].AgencyNumber'),
+        "AgencyName": parse('$.AgencyList[*].AgencyName'),
+        "Preface": parse('$.AgencyList[*].Preface'),
+        "PrefaceText": parse('$.AgencyList[*].Preface.Preface'),
+        "PrefaceSummary": parse('$.AgencyList[*].Preface.PrefaceSummary'),
+        "ChapterList": parse('$.AgencyList[*].ChapterList[*]'),
+        "ChapterNumber": parse('$.AgencyList[*].ChapterList[*].ChapterNumber'),
+        "ChapterName": parse('$.AgencyList[*].ChapterList[*].ChapterName'),
+        "Sections": parse('$.AgencyList[*].ChapterList[*].Sections[*]'),
+        "PartNumber": parse('$.AgencyList[*].ChapterList[*].Sections[*].PartNumber'),
+        "PartName": parse('$.AgencyList[*].ChapterList[*].Sections[*].PartName'),
+        "ArticleNumber": parse('$.AgencyList[*].ChapterList[*].Sections[*].ArticleNumber'),
+        "ArticleName": parse('$.AgencyList[*].ChapterList[*].Sections[*].ArticleName'),
+        "SectionNumber": parse('$.AgencyList[*].ChapterList[*].Sections[*].SectionNumber'),
+        "SectionTitle": parse('$.AgencyList[*].ChapterList[*].Sections[*].SectionTitle'),
+        "Body": parse('$.AgencyList[*].ChapterList[*].Sections[*].Body'),
+        "Authority": parse('$.AgencyList[*].ChapterList[*].Sections[*].Authority'),
+        "HistoricalNote": parse('$.AgencyList[*].ChapterList[*].Sections[*].HistoricalNote')
+    }
+    
 
     # Administrative Code Data
     titles = fetch_data(BASE_URL + "AdministrativeCodeGetTitleListOfJson")
     if titles is not None:
-        all_data["AdministrativeCodeGetTitleListOfJson"] = titles
-
         for title in titles:
             title_number = title["TitleNumber"]
-            agencies = fetch_data(BASE_URL + f"AdministrativeCodeGetAgencyListOfJson/{title_number}")
-            if agencies is not None:
-                all_data.setdefault("AdministrativeCodeGetAgencyListOfJson", {})[title_number] = agencies
-
-                for agency in agencies.get("AgencyList", []):
+            title_name = title["TitleName"]
+            agencies_fetched = fetch_data(BASE_URL + f"AdministrativeCodeGetAgencyListOfJson/{title_number}")
+            if agencies_fetched is not None:
+                agencies = agencies_fetched.get("AgencyList", [])
+                for agency in agencies:
                     agency_number = agency["AgencyNumber"]
                     preface = fetch_data(BASE_URL + f"AdministrativeCodePrefaceJson/{title_number}/{agency_number}")
                     if preface is not None:
-                        all_data.setdefault("AdministrativeCodePrefaceJson", {})[f"{title_number}_{agency_number}"] = preface
+                        preface.pop("TitleNumber", None)
+                        preface.pop("AgencyNumber", None)
+                        preface.pop("TitleName", None)
+                        preface.pop("AgencyName", None)
+                        agency.update({"Preface": preface})
+                        #all_data.setdefault("AdministrativeCodePrefaceJson", {})[f"{title_number}_{agency_number}"] = preface
 
-                    chapters = fetch_data(BASE_URL + f"AdministrativeCodeChapterListOfJson/{title_number}/{agency_number}")
-                    if chapters is not None:
-                        all_data.setdefault("AdministrativeCodeChapterListOfJson", {})[f"{title_number}_{agency_number}"] = chapters
+                    chapters_obj = fetch_data(BASE_URL + f"AdministrativeCodeChapterListOfJson/{title_number}/{agency_number}")
+                    if chapters_obj is not None:
+                        jsonpath_expression = parse('$.AgencyList')
+                        matches = jsonpath_expression.find(chapters_obj)
+                        #all_data.setdefault("AdministrativeCodeChapterListOfJson", {})[f"{title_number}_{agency_number}"] = chapters
+                        for agency_fetched in chapters_obj["AgencyList"]:
+                            if agency_fetched["AgencyNumber"] == agency_number:
+                                agency_fetched.pop("AgencyNumber", None)
+                                agency_fetched.pop("AgencyName", None)
+                                chapter_list = agency_fetched["ChapterList"]
+                                for chapter in chapter_list:
+                                    chapter_number = chapter["ChapterNumber"]
+                                    sections_obj = fetch_data(BASE_URL + f"AdministrativeCodeGetSectionListOfJson/{title_number}/{agency_number}/{chapter_number}")
+                                    if sections_obj is not None:
+                                        #use jsonpath to extract "Sections" data from JSON response with schema above.
+                                        sections_matches = jsonpath_expressions["Sections"].find(sections_obj)
+                                        # Use the JSONPath expression to find and extract sections data
+                                        sections = [match.value for match in sections_matches]
+                                        chapter["Sections"] = sections
+                                        for section in sections:
+                                            section_number = section["SectionNumber"]
 
-                        for chapter in chapters.get("ChapterList", []):
-                            chapter_number = chapter.get("ChapterNumber")
-                            if chapter_number:
-                                sections = fetch_data(BASE_URL + f"AdministrativeCodeGetSectionListOfJson/{title_number}/{agency_number}/{chapter_number}")
-                                if sections is not None:
-                                    all_data.setdefault("AdministrativeCodeGetSectionListOfJson", {})[f"{title_number}_{agency_number}_{chapter_number}"] = sections
-
-                                    for section in sections.get("SectionList", []):
-                                        section_number = section.get("SectionNumber")
-                                        if section_number:
-                                            section_details = fetch_data(BASE_URL + f"AdministrativeCodeGetSectionDetailsJson/{title_number}/{agency_number}/{chapter_number}/{section_number}/0/0")
-                                            if section_details is not None:
-                                                all_data.setdefault("AdministrativeCodeGetSectionDetailsJson", {})[f"{title_number}_{agency_number}_{chapter_number}_{section_number}"] = section_details
-
+                                            '''	
+                                            This service returns a the Administratve Section detail by 'titleNumber', 'agencyNumber', 'chapterNumber', 'sectionNumber', 'sectionNumberPoint', 'sectionNumberColon' (if no point or colon, pass a 0 for each value)
+                                            but there is no refernce to the API calls for how to get 'sectionNumberPoint', 'sectionNumberColon' so we are using zeros as a placeholder for now.
+                                            '''
+                                            
+                                            sectionNumberPoint = 0
+                                            sectionNumberColon = 0
+                                                                              
+                                            section_details_obj =  fetch_data(BASE_URL + f"AdministrativeCodeGetSectionDetailsJson/{title_number}/{agency_number}/{chapter_number}/{section_number}/{sectionNumberPoint}/{sectionNumberColon}")
+                                            if section_details_obj is not None:
+                                                #use jsonpath to extract "Body" data from JSON response with schema above
+                                                section_detail_matches = jsonpath_expressions["Body"].find(section_details_obj)
+                                                # Use the JSONPath expression to find and extract section detail data
+                                                section_detail = [match.value for match in section_detail_matches]
+                                                section["Body"] = section_detail[0]
+                                    chapter.update({"Sections":sections})   
+                    agency.update({"ChapterList":chapter_list}) 
+            title.update({"AgencyList":agencies})
+    return titles
+    
+def gather_authorities_data():
+    # dict to hold all data in new schema
+    authorities = []
     # Authorities Data
     authorities = fetch_data(BASE_URL + "AuthoritiesGetListOfJson")
     if authorities is not None:
-        all_data["AuthoritiesGetListOfJson"] = authorities
-
         for authority in authorities:
             short_name = authority.get("ShortName")
             if short_name:
                 authority_detail = fetch_data(BASE_URL + f"AuthoritiesGetDetailJson/{short_name}")
                 if authority_detail is not None:
-                    all_data.setdefault("AuthoritiesGetDetailJson", {})[short_name] = authority_detail
+                    authority["Body"] = authority_detail["Body"]
+    return authorities
 
+def gather_charters_data():
     # Charters Data
+    charters = []
     charters = fetch_data(BASE_URL + "ChartersGetListOfJson")
     if charters is not None:
-        all_data["ChartersGetListOfJson"] = charters
-
         for charter in charters:
             short_name = charter.get("ShortName")
             if short_name:
                 charter_detail = fetch_data(BASE_URL + f"ChartersGetDetailJson/{short_name}")
                 if charter_detail is not None:
-                    all_data.setdefault("ChartersGetDetailJson", {})[short_name] = charter_detail
+                    charter["Body"] = charter_detail["Body"]
+    return charters
 
+def gather_statute_data():
+    all_data ={}
     # Code of Virginia Data
     titles = fetch_data(BASE_URL + "CoVTitlesGetListOfJson")
     if titles is not None:
@@ -111,8 +263,10 @@ def gather_all_data():
                                             if section_details is not None:
                                                 all_data.setdefault("CoVSectionsGetSectionDetailsJson", {})[section_number] = section_details
                                 print(section)
-
+    return all_data
+def gather_compacts_data():
     # Compacts Data
+    all_data ={}
     compacts = fetch_data(BASE_URL + "CompactsTitlesGetListOfJson")
     if compacts is not None:
         all_data["CompactsGetListOfJson"] = compacts
@@ -123,8 +277,11 @@ def gather_all_data():
                 compact_detail = fetch_data(BASE_URL + f"CompactSectionsGetSectionDetailsJson/{short_name}")
                 if compact_detail is not None:
                     all_data.setdefault("CompactSectionsGetSectionDetailsJson", {})[short_name] = compact_detail
+    return all_data
 
     # Constitution Data
+def gather_constitution_data():
+    all_data ={}
     articles = fetch_data(BASE_URL + "ConstitutionArticlesGetListOfJson")
     if articles is not None:
         all_data["ConstitutionArticlesGetListOfJson"] = articles
@@ -142,9 +299,11 @@ def gather_all_data():
                             section_details = fetch_data(BASE_URL + f"ConstitutionSectionDetailsJson/{article_number}/{section_number}")
                             if section_details is not None:
                                 all_data.setdefault("ConstitutionSectionDetailsJson", {})[f"{article_number}_{section_number}"] = section_details
+    return all_data
 
+def gather_unified_code_data():
     # Uncodified Acts Data
-    
+    all_data ={}
     current_year = datetime.now().year 
     years = list(range(1946, current_year + 1))
     for year in years:
@@ -154,11 +313,54 @@ def gather_all_data():
 
     return all_data
 
-def save_data_as_per_schema(data):
-    with open('comprehensive_VA_code.json', 'w', encoding='utf-8') as f:
+def save_data_as_per_schema(data, path, filename):
+    os.makedirs(path, exist_ok=True)
+    with open(os.path.join(path, filename+'.json'), 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
     print("All data has been successfully fetched and saved in 'comprehensive_VA_code.json'")
 
 if __name__ == "__main__":
-    all_data = gather_all_data()
-    save_data_as_per_schema(all_data)
+    #Get the path of the current script
+    current_script_path = os.path.realpath(__file__)
+    # Get the directory of the current script
+    current_directory = os.path.dirname(current_script_path)
+    # Define the subdirectory name
+    subdirectory_name = "json_code_data"
+    # Create the path for the subdirectory
+    subdirectory_path = os.path.join(current_directory, subdirectory_name)
+    
+    #gather admin data
+    administrative_code = gather_administrative_code()
+    filename = "admin_code_data_" + datetime.now().strftime("%Y%m%d_%H%M%S")
+    save_data_as_per_schema(administrative_code, subdirectory_path, filename)
+    
+    #gather the authorities data
+    authorities_data = gather_authorities_data()
+    filename = "authorities_data_" + datetime.now().strftime("%Y%m%d_%H%M%S")
+    save_data_as_per_schema(authorities_data, subdirectory_path, filename)
+    
+    #gather charters data
+    charters_data = gather_charters_data()
+    filename = "charters_data_" + datetime.now().strftime("%Y%m%d_%H%M%S")
+    save_data_as_per_schema(charters_data, subdirectory_path, filename)
+    
+    #gather statute data
+    statute_data = gather_statute_data()
+    filename = "statute_data_" + datetime.now().strftime("%Y%m%d_%H%M%S")
+    save_data_as_per_schema(statute_data, subdirectory_path, filename)
+    
+    #gather compacts data
+    compacts_data = gather_compacts_data()
+    filename = "compacts_data_" + datetime.now().strftime("%Y%m%d_%H%M%S")
+    save_data_as_per_schema(compacts_data, subdirectory_path, filename)
+    
+    #gather constitution data
+    constitution_data = gather_constitution_data()
+    filename = "constitution_data_" + datetime.now().strftime("%Y%m%d_%H%M%S")
+    save_data_as_per_schema(constitution_data, subdirectory_path, filename)
+    
+    #gather unified code data
+    unified_code_data = gather_unified_code_data()
+    filename = "unified_code_data_" + datetime.now().strftime("%Y%m%d_%H%M%S")
+    save_data_as_per_schema(unified_code_data, subdirectory_path, filename)
+    
